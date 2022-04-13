3481d5aec1fd19b69312d9875d84025c70eb17a0
fernandesitalo <italofernandes.fg@gmail.com>
2022-04-13 08:24:39 -0300

    output instances

--- kTSP/run_ktsp.sh
+++ kTSP/run_ktsp.sh
@@ -6,6 +6,6 @@ for ((v=100; v<=250; v+=50))
 do
 v2=$(( $v / 2 ))
 python3 ktsp.py $v 0 $filename > output_instances/V$underline$v/$arq_saida$v$underline"k0".txt
-python3 ktsp.py $v $v2 $filename > output_instances/V$underline$v/$arq_saida$v$underline"k"$v2.txt
 python3 ktsp.py $v $v $filename > output_instances/V$underline$v/$arq_saida$v$underline"k"$v.txt
+python3 ktsp.py $v $v2 $filename > output_instances/V$underline$v/$arq_saida$v$underline"k"$v2.txt
 done
